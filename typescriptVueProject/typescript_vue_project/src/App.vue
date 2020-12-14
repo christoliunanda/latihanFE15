371c1af0949c4b7ddf31f26b8312e96e5fa94795
christoliunanda <75774085+christoliunanda@users.noreply.github.com>
2020-12-14 19:11:04 +0700

    Update App.vue

--- typescriptVueProject/typescript_vue_project/src/App.vue
+++ typescriptVueProject/typescript_vue_project/src/App.vue
@@ -2,8 +2,11 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      
       <router-link to="/about">About</router-link> 
-
+      <!-- |<router-link to="/registration">Registration</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/logout">Logout</router-link>  -->
     
     </div>
     <router-view/>
