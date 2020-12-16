0956907efecaf4007a5c5b1c72820e2ece2c20a0
christoliunanda <75774085+christoliunanda@users.noreply.github.com>
2020-12-16 13:21:46 +0700

    Create Login.vue

---
+++ perpustakaan/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+
+
+export default class Login extends Vue {
+
+
+}
+</script>
