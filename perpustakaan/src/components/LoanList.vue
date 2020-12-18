429b8cadf215c7151a7a4e8e7d3abca29246c48c
christoliunanda <75774085+christoliunanda@users.noreply.github.com>
2020-12-18 10:18:28 +0700

    Adding bootstrap cdn

---
+++ perpustakaan/src/components/LoanList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="loan-list p-1 p-md-5">
+</template>
+
+<script lang="ts">
+
+    import {Component,Vue} from 'vue-property-decorator';
+
+    import StatusCode from "../../common/StatusCode"
+
+    import Book from "../../entity/Book";
+    import Loan from '../../entity/Loan';
+
+    @Component({components: {CustomTable}})
+    export default class BookList extends Vue{
+        
+        public baseApi: string = "http://202.152.159.164:8088/perpus/";
+
+        public entity: new () => Loan = Loan;
+
+        public books: Array<Book> = [];
+
+        public rows: number = 0;
+
+        publis isBeingRequest: boolean = false;
+
+        public get user(): User{
+        }
+
+        public doValidate(loan: Loan){
+        }
+
+        public onChangeBook(value: string, loan: Loan){
+        }
+
+        
+
+
+
+    }
+
+</script>
