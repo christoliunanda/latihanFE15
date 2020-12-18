8bde437403159380fe7824427b0d8ab9dfdf2f37
christoliunanda <75774085+christoliunanda@users.noreply.github.com>
2020-12-18 12:51:46 +0700

    Update Login.vue

--- perpustakaan/src/views/Login.vue
+++ perpustakaan/src/views/Login.vue
@@ -5,9 +5,11 @@
 </template>
 
 <script lang="ts">
+import Axios, {AxiosResponse, AxiosError} from "axios";
 import { Component, Vue } from 'vue-property-decorator';
 
-import User from "./../entity/User"
+import User from "./../entity/User";
+import StatusCode from "./../common/StatusCode";
 
 export default class Login extends Vue {
   
@@ -25,6 +27,24 @@ export default class Login extends Vue {
     let valid: boolean = this.user.username !== "" && this .user.password !== "";
 
     if (this.isRegister) {
+      
+    }
+
+    if(valid){
+      const baseApi: string = "http://202/152.159.164:8088/perpus/";
+      const url: string = `${baseApi}auth/${this.isRegister ? 'do-register' : 'do-login'}`;
+
+      Axios.post(url, this.user.serialize(), {responseType: 'json'}).then((response: AxiosResponse) =>{
+        if(response && response.data && response.data.status === StatusCode.LOGIN_SUCCESS){
+          const user: User = User.InstanceFrom(response.data.data);
+
+          console.log(user);
+          console.log(user instanceof User);
+        }
+
+      }).catch((error: AxiosError) =>{
+          console.error(error);
+      });
 
     }
   }
